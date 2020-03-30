38789de25ff8724dca08d2499bf1801ea823a0d0
Максим Сиволапов <maxsergo130075@gmail.com>
2020-03-30 13:52:02 +0300

    Reacr Router

--- src/components/MainComponent.js
+++ src/components/MainComponent.js
@@ -1,8 +1,11 @@
 import React, { Component } from 'react';
-import {Navbar, NavbarBrand} from 'reactstrap';
 import Menu from './MenuComponent';
 import {DISHES} from '../shared/dishes';
 import DishDetail from './DishdetailComponent';
+import Header from'./HeaderComponent';
+import Footer from './FooterComponent';
+import Home from './HomeComponent';
+import {Switch, Route, Redirect} from 'react-router-dom';
 
 class Main extends Component{
   constructor(props){
@@ -13,21 +16,23 @@ class Main extends Component{
     };
   }
 
-  onDishSelect(dishId){
-    this.setState({selectedDish: dishId});
-}
+
 
   render(){
+    const HomePage = ()=>{
+      return(
+        <Home></Home>
+      )
+    }
     return(
       <div key="root">
-      <Navbar dark color="primary">
-        <div className="container">
-          <NavbarBrand href="/">Ristorante Con Fusion</NavbarBrand>
-        </div>
-      </Navbar>
-      <Menu dishes={this.state.dishes}
-      onClick={(dishId)=>this.onDishSelect(dishId)}></Menu>
-      <DishDetail dish={this.state.dishes.filter((dish)=>dish.id===this.state.selectedDish)[0]}></DishDetail>
+        <Header></Header>
+        <Switch>
+          <Route path="/home" component={HomePage}></Route>
+          <Route exact path="/menu" component={() => <Menu dishes={this.state.dishes}></Menu>}></Route>
+          <Redirect to="/home"></Redirect>
+        </Switch>
+        <Footer></Footer>
     </div>
     )
   }
